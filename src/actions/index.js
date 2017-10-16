fadbcea45975b4adb1b4d994a25d2f888826611c
junhoje <jje@calstatela.edu>
2017-10-16 23:01:45 +0900

    redux

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const MULTIPLY = 'MULTIPLY';
+export const DIVIDE = 'DIVIDE';
+export const SET_DIFF = 'SET_DIFF';
+
+export function increment() {
+    return {
+        type: INCREMENT
+    };
+}
+
+export function decrement() {
+    return {
+        type: DECREMENT
+    };
+}
+export function multiply() {
+    return {
+        type: MULTIPLY
+    };
+}
+export function divide() {
+    return {
+        type: DIVIDE
+    };
+}
+
+export function setDiff(value) {
+    return {
+        type: SET_DIFF,
+        diff: value
+    };
+}
---
+++ src/components/Buttons.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement, multiply,divide } from '../actions';
+
+class Buttons extends React.Component {
+    render() {
+        return (
+            <div>
+                <button type="button"
+                        onClick={ this.props.onIncrement }>
+                        +
+                </button>
+                <button type="button"
+                        onClick={ this.props.onDecrement }>
+                        -
+                </button>
+                <button type="button"
+                        onClick={ this.props.onMultiply }>
+                        *
+                </button>
+                <button type="button"
+                        onClick={ this.props.onDivide }>
+                        /
+                </button>
+            </div>
+        )
+    }
+}
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        onIncrement: () => dispatch(increment()),
+        onDecrement: () => dispatch(decrement()),
+        onMultiply: () => dispatch(multiply()),
+        onDivide: () => dispatch(divide())
+    }
+}
+
+Buttons = connect(undefined, mapDispatchToProps)(Buttons);
+
+export default Buttons;
